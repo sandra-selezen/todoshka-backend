2599bdbafe67fd7dfc9cd93c3ae96a8ce54cdf81
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-23 14:10:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { DB_HOST } = process.env
 
 mongoose.connect(DB_HOST)
   .then(() => {
-    app.listen(3000)
+    app.listen(5000)
     console.log('Database connection successful')
   })
   .catch((error) => {
